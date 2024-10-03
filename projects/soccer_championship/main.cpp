ea3264ff5e4d7a5c6903774f810cec5d2857d165
MicaelTargino <micaelgomestargino@gmail.com>
2024-10-03 11:54:24 -0300

    setup new project

---
+++ projects/soccer_championship/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ostream;
+using std::istream;
+
+
+int main(void) {
+    return 0;
+}
