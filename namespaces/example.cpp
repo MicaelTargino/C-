399b8040547817f1e639b2afac37fdf9fe4f46dd
MicaelTargino <micaelgomestargino@gmail.com>
2024-10-10 11:53:56 -0300

    update math namespace

--- namespaces/example.cpp
+++ namespaces/example.cpp
@@ -4,14 +4,20 @@ namespace math {
     int sum(int a, int b) {
         return a+b;
     }
+    int sub(int a, int b) {
+        return a-b;
+    }
 }  
 
+using math::sub;
+
 int main() {
-    int x,a,b;
+    int x,y,a,b;
     a=10;
     b=20;
 
     x = math::sum(a,b);
+    y = sub(a,b);
 
     std::cout << x << std::endl;
 }
